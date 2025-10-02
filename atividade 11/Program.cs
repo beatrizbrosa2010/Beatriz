211d2dc0a5acb151feaaef2cf2818c06258ebf6a
beatrizbrosa2010 <beatrizbrosa8@gmail.com>
2025-10-02 16:51:51 -0300

    Add files via upload

---
+++ atividade 11/Program.cs
@@ -0,0 +1,28 @@
+﻿//declaracao de variavel
+
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Ano atual:");
+        int anoAtual = int.Parse(Console.ReadLine() ?? "0");
+
+        Console.Write("Ano de nascimento: ");
+        int anoNascimento = int.Parse(Console.ReadLine() ?? "0");
+
+        int idade = anoAtual - anoNascimento;
+
+        if (idade < 16)
+            Console.WriteLine("Nao vota.");
+        else if (idade >=16)
+            Console.WriteLine("Voto facultativo.");
+        else if (idade >= 18 && idade <= 69)
+            Console.WriteLine("RVoto obrigatorio");
+        else // idade >=70
+            Console.WriteLine();
+        Console.WriteLine("Pressione Enter para sair.");
+        Console.ReadLine();
+    }
+}
